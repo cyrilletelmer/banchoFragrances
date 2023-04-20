f5b43a4a8dac784245ead06f922df6de79a213b1
cyrilletelmer <38984789+cyrilletelmer@users.noreply.github.com>
2023-04-20 22:33:28 +0200

    Add files via upload

--- Transactions/Model/Warning.php
+++ Transactions/Model/Warning.php
@@ -2,10 +2,18 @@
 
 class Warning
 	{
-	const TYPE_EXCESS_AMOUNT = "EXCESS_AMOUNT";
+	
 	const TYPE_LACKS_BASE = "LACKS_BASE";
 	const TYPE_LACKS_MIDDLE = "LACKS_MIDDLE";
 	const TYPE_LACKS_TOP = "LACKS_TOP";
+	const TYPE_STRONG_BASE = "STRONG_BASE";
+	const TYPE_WEAK_BASE = "WEAK_BASE";
+	const TYPE_STRONG_MIDDLE = "STRONG_MIDDLE";
+	const TYPE_WEAK_MIDDLE = "WEAK_MIDDLE";
+	const TYPE_STRONG_TOP = "STRONG_TOP";
+	const TYPE_WEAK_TOP = "WEAK_TOP";
+	const TYPE_EXCESS_AMOUNT = "EXCESS_AMOUNT";
+	const TYPE_LACK_AMOUNT = "LACK_AMOUNT";
 	
 	public string $mType;
 	public ?int $mTargetOfWarning;
